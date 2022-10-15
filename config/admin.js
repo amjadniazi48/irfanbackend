78b69d2388a7e46d1c9aecb19e1f506a2e5e1d66
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2022-10-15 19:10:14 +0500

    upgraded

--- config/admin.js
+++ config/admin.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   apiToken:{
-    salt: env('API_TOKEN_SALT=86295706766ad1de48e48666130067c5')
+    salt: env('API_TOKEN_SALT','86295706766ad1de48e48666130067c5')
   },
   auth: {
     secret: env('ADMIN_JWT_SECRET', '1aca2905d8b858e4eea1db0c8f093b65'),
